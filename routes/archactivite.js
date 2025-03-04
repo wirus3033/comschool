ab93de03fc0aeecdd3ddd237651e20f031204f8e
SAMBITIANA <107566445+SAMBITIANA@users.noreply.github.com>
2025-03-04 10:46:54 +0300

    api

--- routes/archactivite.js
+++ routes/archactivite.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { db } = require("../config/serverConfig");
 
 router.get("/", async (req, res) => {
-  const query = "SELECT * FROM ARCHAtivite";
+  const query = "SELECT * FROM ARCHActivite";
 
   try {
     const [results] = await db.promise().query(query);
